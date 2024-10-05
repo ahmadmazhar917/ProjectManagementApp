956b9e20b3dd1f0855a602ca24195c848a0f437e
ahmadmazhar917 <ahmadmazhar917@gmail.com>
2024-10-05 17:52:52 +0500

    Build Sidebar, NewProject Page and FallBack Page

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import NewProject from "./components/NewProject";
+import Sidebar from "./components/Sidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+  return (
+    <main className="my-8 h-screen flex gap-8">
+      <Sidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
+    </main>
+  );
+}
+
+export default App;
